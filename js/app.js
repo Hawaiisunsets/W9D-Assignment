b354d1a79daef91074034d32714917453cd77dff
victor <kwoksters@hotmail.com>
2022-11-07 13:55:35 -0700

    updated js file

--- js/app.js
+++ js/app.js
@@ -7,14 +7,16 @@ let userAges = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 let counter = 0;
 while (counter < userAges.length){
     // userAges at least 18 years old
-    if (userAges[counter] >= 18)
-    // display userNames
+    if (userAges[counter] >= 18){
+// display userNames
     console.log(userNames[counter]);
-    // counter +1 so it ends
+// counter +1 so it ends
+    }
     counter = counter +1;
 }
 // counter ++ is the same as counter +1
 for(let counter = 0; counter < userAges.length; counter++){
-    if (userAges[counter] < 18)
+    if (userAges[counter] < 18){
     console.log(userNames[counter]);
+    }
 }
